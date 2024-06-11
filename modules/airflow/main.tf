e181fd564bb185c2b355e52529f0c5c64720358b
Dylan Watt <dylannwatt@gmail.com>
2024-06-11 11:15:28 -0400

    Add exposures for services

--- modules/airflow/main.tf
+++ modules/airflow/main.tf
@@ -4,3 +4,23 @@ resource "helm_release" "airflow" {
   chart      = "airflow"
   version    = "8.8.0"
 }
+
+resource "kubernetes_service" "expose_airflow_webeserver" {
+  metadata {
+    name = "expose-airflow-webeserver"
+  }
+
+  spec {
+    type = "NodePort"
+    port {
+      port        = 8080
+      target_port = 8080
+      node_port   = 30081
+    }
+
+    selector = {
+      app= "airflow"
+      component= "web"
+    }
+  }
+}

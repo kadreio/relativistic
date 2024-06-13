9e32b49c487feac20a7e44c9f233d446dc52a3c0
Dylan Watt <dylannwatt@gmail.com>
2024-06-13 18:54:00 -0400

    Add windmill

---
+++ modules/windmill/main.tf
@@ -0,0 +1,26 @@
+resource "helm_release" "windmill" {
+  name       = "windmill"
+  repository = "https://windmill-labs.github.io/windmill-helm-charts"
+  chart      = "windmill"
+  version    = "2.0.214"
+  values     = [file("./${path.module}/helm_values.yaml")]
+}
+
+resource "kubernetes_service" "expose_windmill_webserver" {
+  metadata {
+    name = "expose-workflows-webserver"
+  }
+
+  spec {
+    type = "NodePort"
+    port {
+      port        = 8000
+      target_port = 8000
+      node_port   = 30085
+    }
+
+    selector = {
+      "app.kubernetes.io/name" = "windmill-app"
+    }
+  }
+}

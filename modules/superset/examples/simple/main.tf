de74b475cf38e27d21b0133f524495680e6a3679
Dylan Watt <dylan@kadre.io>
2024-10-24 17:18:08 -0400

    More superset docs

--- modules/superset/examples/simple/main.tf
+++ modules/superset/examples/simple/main.tf
@@ -10,7 +10,17 @@ provider "helm" {
 
 module "superset" {
     source = "kadreio/relativistic/kubernetes//modules/superset"
+    #The key used for superset cookies
     superset_secret_key = random_string.cookie_key.result
+
+    #The default user for superset, set this to the first admin's email
+    superset_default_user = "admin@example.com"
+
+    #The default password for that admin user, change this after initial login
+    superset_default_password = "password"
+
+    #The port that the superset service will be exposed on for local access
+    superset_local_exposed_port = 30086
 }
 
 resource "random_string" "cookie_key" {

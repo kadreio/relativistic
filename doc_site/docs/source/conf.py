79f4620d0e874b17c7a4b2a8f59fb4da9cc15db7
Dylan Watt <dylan@kadre.io>
2024-10-28 15:05:28 -0400

    Fixup tf pixel

--- doc_site/docs/source/conf.py
+++ doc_site/docs/source/conf.py
@@ -125,7 +125,7 @@ html_theme_options = {
         },
         "Terraform": {
             "link": "https://registry.terraform.io/modules/kadreio/relativistic/kubernetes/latest",
-            "icon": '<svg width="26px" height="26px" viewBox="12 12 40 40g" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M38.06 26.151v11.473L48 31.891V20.406l-9.94 5.745z" fill="#4040B2"/><path d="m27.03 20.406 9.94 5.745v11.473l-9.94-5.74V20.407zM16 14v11.479l9.94 5.74v-11.48L16 14zm11.03 30.624 9.94 5.74v-11.48l-9.94-5.739v11.48z" fill="#5C4EE5"/></svg>'
+            "icon": '<svg width="26px" height="26px" viewBox="12 12 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M38.06 26.151v11.473L48 31.891V20.406l-9.94 5.745z" fill="#4040B2"/><path d="m27.03 20.406 9.94 5.745v11.473l-9.94-5.74V20.407zM16 14v11.479l9.94 5.74v-11.48L16 14zm11.03 30.624 9.94 5.74v-11.48l-9.94-5.739v11.48z" fill="#5C4EE5"/></svg>'
         },
     },
     "page_width": "100%",

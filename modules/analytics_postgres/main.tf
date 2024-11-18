2f033fa92dd3142c3b51303467b0d7d3dde1b46c
Dylan Watt <dylan@kadre.io>
2024-11-18 18:00:43 -0500

    logically

--- modules/analytics_postgres/main.tf
+++ modules/analytics_postgres/main.tf
@@ -10,4 +10,14 @@ resource "helm_release" "analytics_postgres" {
   chart      = "postgresql"
   version    = var.analytics_postgres_chart_version
   force_update = true
+  values     = [
+    file("${path.module}/helm_values.yaml"),
+    var.override_helm_values
+  ]
+}
+
+variable "override_helm_values" {
+  description = "Override helm values"
+  type        = string
+  default     = ""
 }
